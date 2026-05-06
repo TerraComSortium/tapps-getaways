f251c08eff10e994892fbfe9faeba60809ac00c1
samanthazr <samantha.zambrana@racquetsapp.com>
2026-05-06 18:00:46 -0400

    Fix(api):call logic and errors for Axios interceptor(auth token)

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,17 +1,18 @@
 import axios from "axios";
-import {auth} from '../lib/firebase';
+import { auth } from '../lib/firebase';
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
 });
 api.interceptors.request.use(
   async (config:any) => {
     // const token = localStorage.getItem("token");
-
     const user = auth.currentUser;
     if(user){
       const token = await user.getIdToken();//get or refresh token
       config.headers.Authorization = `Bearer ${token}`;
+    } else {
+      console.warn("No user found with interceptor");
     }
     return config;
   },

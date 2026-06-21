0093bde8a834d6e20b6fc9c569aac000e86fe0ed
samanthazr <samantha.zambrana@racquetsapp.com>
2026-06-21 07:45:44 -0400

    Update(AdminSidebar):responsive adjustment

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -1,8 +1,10 @@
 import { Box, Button, IconButton, CircularProgress } from '@mui/material';
 import Grid from '@mui/material/Grid2';
-import AddIcon from '@mui/icons-material/Add';
 import SportsTennisIcon from '@mui/icons-material/SportsTennis';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import NoteAltIcon from '@mui/icons-material/NoteAlt';
+import AddIcon from '@mui/icons-material/Add';
+
 import { useAuth } from '../contexts/AuthContext';
 import { ROUTES } from '../constants/routes';
 import { Role } from '../constants/roles';
@@ -24,92 +26,95 @@ export default function AdminSideBar() {
         <Box
           sx={{
             display: 'flex',
-            flexDirection: { xs: 'row', sm: 'column' },
-            alignItems: 'center',
-            // justifyContent: 'center',
+            flexDirection: { xs: 'row', sm: 'row', md: 'column' },
+            flexWrap:'wrap',
+            alignItems:  { xs: 'center', sm: 'center' },
+            alignContent: 'start',
+            justifyContent: { xs: 'center', sm: 'center', md: 'center' },
             gap: 1,
-            px: { xs: 2, sm: 0 },
+            px: { xs: 2, sm: 2, md: 1.5 },
             pt: { xs: 1.5, sm: 0 },
             pb: { xs: 1.5, sm: 0 },
             mt: { xs: 0, sm: 2.5 },
-            borderBottom: { xs: '1px solid rgba(0,0,0,0.1)', sm: 'none' },
+            borderBottom: { xs: '1px solid rgba(50, 28, 28, 0.1)', sm: 'none' },
           }}>
+          {/* {role === Role.PLAYER && ( */}
+            <Button
+              component="a"
+              disableElevation variant="contained" aria-label="getaways-offers"
+              href={ROUTES.GETAWAYS}
+              sx={{
+                flex: { xs: 1, sm: 'unset' },
+                minWidth:'155px',
+                width: { xs:'auto', sm:'155px'},
+                mb: { xs: 0, sm: 1 },
+                padding: '5px 0px',
+                overflow: 'hidden',
+                bgcolor: BRAND.primary, color: BRAND.white, borderRadius: '8px', fontWeight: 'medium', textTransform: 'none', whiteSpace: 'nowrap'
+              }}
+            >
+              <IconButton aria-label="getaways-offers" sx={{ color: BRAND.lime }}><SportsTennisIcon /></IconButton>
+              Getaways &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </Button>
+          {/* )} */}
           {role === Role.PLAYER && (
-          <Button
-            component="a"
-            disableElevation
-            variant="contained"
-            href={ROUTES.MY_ORDERS}
-            sx={{
-              flex: { xs: 1, sm: 'unset' },
-              width: { xs: 'auto', sm: '175px' },
-              mb: { xs: 0, sm: 1 },
-              padding: '6px 0px',
-              overflow: 'hidden',
-              bgcolor: BRAND.primary, color: BRAND.white, borderRadius: '8px', fontWeight: 'medium', textTransform: 'none', whiteSpace: 'nowrap'
-            }}
-          >
-            <IconButton aria-label="my-orders" sx={{ color: BRAND.lime, pl: '0' }}><ShoppingBagIcon /></IconButton>
-            My getaways
-          </Button>
+          // {role === 'player' && (
+            <Button
+              component="a"
+              href={ROUTES.MY_ORDERS}
+              disableElevation variant="contained" aria-label="my-getaways-orders"
+              sx={{
+                flex: { xs: 1, sm: 'unset' },
+                minWidth:'155px',
+                width: { xs: 'auto', sm: '155px' },
+                mb: { xs: 0, sm: 1 },
+                padding: '5px 0px',
+                overflow: 'hidden',
+                bgcolor: BRAND.primary, color: BRAND.white, borderRadius: '8px', fontWeight: 'medium', textTransform: 'none', whiteSpace: 'nowrap',
+              }}
+            >
+              <IconButton aria-label="my-orders" sx={{ color:BRAND.lime, pl:'0' }}><ShoppingBagIcon /></IconButton>
+              My getaways
+            </Button>
           )}
-          {role === Role.PLAYER && (
-          <Button
-            component="a"
-            disableElevation
-            variant="contained"
-            href={ROUTES.GETAWAYS}
-            sx={{
-              flex: { xs: 1, sm: 'unset' },
-              width: { xs: 'auto', sm: '175px' },
-              mb: { xs: 0, sm: 1 },
-              padding: '6px 6px',
-              overflow: 'hidden',
-              bgcolor: BRAND.primary, color: BRAND.white, borderRadius: '8px', fontWeight: 'medium', textTransform: 'none', whiteSpace: 'nowrap'
-            }}
-          >
-            <IconButton aria-label="my-getaways-offers" sx={{ color: BRAND.lime, pl: '0' }}><SportsTennisIcon /></IconButton>
-            Getaways offers
-          </Button>
-        )}
-        {role === Role.ADMIN && (
-          <Button
-            component="a"
-            disableElevation
-            variant="contained"
-            href={ROUTES.MY_GETAWAYS}
-            sx={{
-              flex: { xs: 1, sm: 'unset' },
-              width: { xs: 'auto', sm: '160px' },
-              mb: { xs: 0, sm: 1 },
-              padding: '6px 6px',
-              overflow: 'hidden',
-              bgcolor: BRAND.primary, color: BRAND.white, borderRadius: '8px', fontWeight: 'medium', textTransform: 'none', whiteSpace: 'nowrap'
-            }}
-          >
-            <IconButton aria-label="my-getaways-offers" sx={{ color: BRAND.lime, pl: '0' }}><SportsTennisIcon /></IconButton>
-            My getaways
-          </Button>
-        )}
-        {role === Role.ADMIN && (
-          <Button
-            component="a"
-            disableElevation
-            variant="contained"
-            href={ROUTES.CREATE_GETAWAY}
-            sx={{
-              flex: { xs: 1, sm: 'unset' },
-              width: { xs: 'auto', sm: '160px' },
-              mb: { xs: 0, sm: 1 },
-              padding: '6px 8px',
-              overflow: 'hidden',
-              bgcolor: BRAND.primary, color: BRAND.white, borderRadius: '8px', fontWeight: 'medium', textTransform: 'none', whiteSpace: 'nowrap'
-            }}
-          >
-            <IconButton aria-label="create-getaway" sx={{ color: BRAND.lime, pl: '0' }}><AddIcon /></IconButton>
-            New getaway
-          </Button>
-        )}
+          {role === Role.ADMIN && (
+            <Button
+              component="a"
+              disableElevation variant="contained" aria-label="my-getaways-offers"
+              href={ROUTES.MY_GETAWAYS}
+              sx={{
+                flex: { xs: 1, sm: 'unset' },
+                minWidth:'155px',
+                width: { xs: 'auto', sm: '155px' },
+                mb: { xs: 0, sm: 1 },
+                padding: '5px 5px',
+                overflow: 'hidden',
+                bgcolor: BRAND.primary, color: BRAND.white, borderRadius: '8px', fontWeight: 'medium', textTransform: 'none', whiteSpace: 'nowrap'
+              }}
+            >
+              <IconButton aria-label="my-getaways-offers" sx={{ color: BRAND.lime, pl: '0' }}><NoteAltIcon /></IconButton>
+              My getaways
+            </Button>
+          )}
+          {role === Role.ADMIN && (
+            <Button
+              // component="a"
+              href={ROUTES.CREATE_GETAWAY}
+              disableElevation variant="contained" aria-label="create-getaways"
+              sx={{
+                flex: { xs: 1, sm: 'unset' },
+                minWidth:'155px',
+                width: { xs: 'auto', sm: '155px' },
+                mb: { xs: 0, sm: 1 },
+                padding: '5px 0px',
+                overflow: 'hidden',
+                bgcolor: BRAND.primary, color: BRAND.white, borderRadius: '8px', fontWeight: 'medium', textTransform: 'none', whiteSpace: 'nowrap'
+              }}
+            >
+              <IconButton aria-label="create-getaway" sx={{ color: BRAND.lime, pl: '0' }}><AddIcon /></IconButton>
+              New getaway
+            </Button>
+          )}
         </Box>
       </Grid>
     </>

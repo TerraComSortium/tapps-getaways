5bcb7dcdaa8aad02b8b1fca80373e831dc79d284
samanthazr <samantha.zambrana@racquetsapp.com>
2026-06-29 16:02:24 -0400

    Update(GetawaySchedule):Get day from date at each row

--- src/components/GetawaySchedule.tsx
+++ src/components/GetawaySchedule.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
+// import * as React from 'react';
+// import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -8,11 +8,11 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import { Typography, Stack } from '@mui/material';
-import { BRAND } from '../theme/colors';
-import '../App.css'; 
+// import { BRAND } from '../theme/colors';
+import '../App.css';
 
 interface ScheduleItem {
-  day: string;
+  date: string;
   startTime: string;
   endTime: string;
   activity: string;
@@ -22,14 +22,26 @@ interface GetawayScheduleProps {
   schedule?: ScheduleItem[];
 }
 interface Column {
-  id: 'day' | 'activity' | 'location';
+  id: 'date' | 'activity' | 'location';
   label: string;
   minWidth?: number;
   align?: 'right';
   // format?: (value: number) => string;
 }
+
+const getDayOfWeek = (dateString: string) => {
+  if (!dateString) return '';
+  const timestamp = Date.parse(dateString);
+  if (isNaN(timestamp)) {
+    console.warn(`date format invalid: "${dateString}"`);
+    return '';
+  }
+  const date = new Date(timestamp);
+  return new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(date); // 'es-ES' || 'en-US'
+};
+
 const columns: readonly Column[] = [
-  { id: 'day',      label: 'Day',      minWidth: 70 },
+  { id: 'date',     label: 'Date',     minWidth: 70 },
   { id: 'activity', label: 'Activity', minWidth: 140 },
   { id: 'location', label: 'Location', minWidth: 140 },
 ];
@@ -43,12 +55,12 @@ export default function GetawaySchedule({ schedule }: GetawayScheduleProps) {
           <TableHead>
             <TableRow>
               {columns.map((column) => (
-                <TableCell 
+                <TableCell
                 sx={{ maxHeight:10 }}
                 key={column.id} align={column.align} style={{ minWidth: column.minWidth }}
                 >
-                  <Typography variant="body2" 
-                  className='title4' 
+                  <Typography variant="body2"
+                  className='title4'
                   color="text.primary"
                   sx={{ fontWeight: 'bold'}}
                   >{column.label}</Typography>
@@ -58,20 +70,24 @@ export default function GetawaySchedule({ schedule }: GetawayScheduleProps) {
           </TableHead>
           <TableBody>
             {hasSchedule ? (
-              schedule.map((item, index) => (
-                <TableRow hover tabIndex={-1} key={index}>
-                  <TableCell>
-                    <Stack direction="column" spacing={0.2}>
-                      <strong>{item.date}</strong>
-                      <Typography variant="subtitle2" color="text.secondary">
-                        {item.startTime} - {item.endTime}
-                      </Typography>
-                    </Stack>
-                  </TableCell>
-                  <TableCell>{item.activity}</TableCell>
-                  <TableCell>{item.location}</TableCell>
-                </TableRow>
-              ))
+              schedule.map((item, index) => {
+                const dayName = getDayOfWeek(item.date);
+                return(
+                  <TableRow hover tabIndex={-1} key={index}>
+                    <TableCell>
+                      <Stack direction="column" spacing={0.2}>
+                        <strong style={{ textTransform: 'capitalize' }}>{dayName}</strong>
+                        <strong>{item.date}</strong>
+                        <Typography variant="subtitle2" color="text.secondary">
+                          {item.startTime} - {item.endTime}
+                        </Typography>
+                      </Stack>
+                    </TableCell>
+                    <TableCell>{item.activity}</TableCell>
+                    <TableCell>{item.location}</TableCell>
+                  </TableRow>
+                )
+              })
             ) : (
               <TableRow>
                 <TableCell colSpan={3} align="center">
@@ -80,7 +96,7 @@ export default function GetawaySchedule({ schedule }: GetawayScheduleProps) {
                   </Typography>
                 </TableCell>
               </TableRow>
-            )}
+          )}
           </TableBody>
         </Table>
       </TableContainer>

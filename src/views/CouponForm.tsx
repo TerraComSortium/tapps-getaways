d56995f4690f5d8a54ca1493eeb8f2f0fd2dcec1
samanthazr <samantha.zambrana@racquetsapp.com>
2026-08-31 19:05:44 -0400

    Update(CouponForm parent view):Integration of couponForm component, props management including getawayId

--- src/views/CouponForm.tsx
+++ src/views/CouponForm.tsx
@@ -1,27 +1,37 @@
-import CouponForm from '../components/CouponForm';
+import {
+  useNavigate, useParams,
+  useSearchParams
+} from 'react-router-dom';
 import { useCoupon,
   // useCouponActions
-} from '../hooks/useCouponActions';
-import {
-  // useNavigate,
-  useParams, useSearchParams } from 'react-router-dom';
-import AdminSideBar from '../components/AdminSidebar';
+} from '../hooks/useCoupon';
+import { Box, CircularProgress } from '@mui/material';
 import Grid from '@mui/material/Grid2';
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
+import {firestoreToDate} from '../utils/dates';
+import AdminSideBar from '../components/AdminSidebar';
+import CouponForm from '../components/CouponForm';
+
 export default function CouponFormView() {
   const { id } = useParams(); // if(id) → edit mode
   const [searchParams] = useSearchParams();
-  const ofertaId = searchParams.get('ofertaId') ?? undefined;
+  const navigate = useNavigate();
+  // const ofertaId = searchParams.get('ofertaId') ?? undefined;
+  const getawayIdFromUrl = searchParams.get('getawayId') ?? '';
 
   const isEditing = !!id;
 
   //if(edit) -> fetch(coupon to insert presets in fields)
-  const { data: existing, loading } = useCoupon(id); // hook retorna null si !id
+  const { data: existing, loading } = useCoupon(id); //hook return null if !id
+  const handleError = (error: string) => {
+    console.log('CouponFormView: error', error);
+  };
+  const handleSuccess = (redirectPath?: string) => {
+    if (redirectPath) navigate(redirectPath);
+  };
   if (loading) return
-    <Box sx={{ display: 'flex' }}>
-      <CircularProgress aria-label="Loading…" />
-    </Box>;
+  <Box sx={{ display: 'flex' }}>
+    <CircularProgress aria-label="Loading…" /> {/* t('common.loading') */}
+  </Box>;
 
   // TODO: add useCoupon(id) when getCoupon endpoint available
   return (
@@ -30,10 +40,24 @@ export default function CouponFormView() {
       <Grid size={{ xs: 12, sm: 9, md: 10 }} className='section blueBg'>
       <CouponForm
         mode={isEditing ? 'edit' : 'create'}
-        initialValues={{ ofertaId }}
+        // initialValues={{ ofertaId }}
         // initialValues={existing ?? { ofertaId }} //this with getCoupon endpoint available
+        initialValues={existing ? {
+          startDate: firestoreToDate(existing.validFrom),
+          endDate: firestoreToDate(existing.validUntil),
+          userLimit: existing.userLimit,
+          couponCode: existing.title,
+          description: existing.description,
+          amount: existing.discount,
+          discountType: existing.discountType,
+          getawayId: existing.getawayId,
+        } : {
+          getawayId: getawayIdFromUrl, //POST
+        }}
         couponId={id}
-        />
+        onSuccess={handleSuccess}
+        onError={handleError}
+      />
       </Grid>
     </Grid>
   );
